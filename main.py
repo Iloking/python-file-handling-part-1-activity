7b6b193fe55731fcbbcb39b15c9041a86b2f6ecf
Iloking <halylish@gmail.com>
2024-10-05 15:13:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-file=open("file.txt","r")
-print(file.read())
+file = open('Codingal.txt', 'r')
+content = file.read()
+print(content)
 file.close()
+
+print("\n=====================\n")
+
+file_8_char = open('Codingal.txt', 'r')
+content_8_char = file_8_char.read(8)
+print(content_8_char)
+file_8_char.close()
